639501811275b45c0eefa1b58a609d4de833498b
nourshalann77 <nourashrafshalan@gmail.com>
2025-01-25 14:42:28 +0200

    nonoProject

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './components/home/Home'
+
+export default function App() {
+  return (
+   <>
+ 
+   <h1 className='bg-primary text-center underline'> Lorem, ipsum.</h1>
+   < Home/>
+   </>
+  )
+}
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-right bg-orange-600'> hey noni</h1>
+    </div>
+  )
+}
