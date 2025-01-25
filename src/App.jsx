5332846f663ad5f27043c5d748eb88fa72d24f89
nourshalann77 <nourashrafshalan@gmail.com>
2025-01-25 14:59:29 +0200

    h3

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './components/home/Home'
 export default function App() {
   return (
    <>
- 
+   <h3>ay haga</h3>
    <h1 className='bg-primary text-center underline'> Lorem, ipsum.</h1>
    < Home/>
    </>
